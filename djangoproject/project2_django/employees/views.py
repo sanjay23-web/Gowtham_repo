ca872061a4d85f6bc608cd61b472bb4ac1102f41
dxgowtham40@gmail.com <dxgowtham40@gmail.com>
2026-05-01 13:20:46 +0530

    Djangoenployeedirectory

---
+++ djangoproject/project2_django/employees/views.py
@@ -0,0 +1,13 @@
+
+from django.http import HttpResponse
+from employees.models import Employee
+from django.shortcuts import get_object_or_404, render
+
+# Create your views here.
+
+def employee_detail(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    context = {
+        'employee': employee,
+    }
+    return render(request, 'employee_detail.html', context)
